624ccb1fa7bfbc219a5758dd3eaff26c10cb4392
Nekhai <NekhaiYurii@gmail.com>
2021-10-11 10:50:05 +0300

    feat: tickets calculator

--- museum/js/tickets.js
+++ museum/js/tickets.js
@@ -17,3 +17,38 @@ booking.addEventListener('click', hideBooking);
 form.addEventListener('click', function(event) {
   event.stopPropagation()
 });
+
+
+let ticketPrice = 20;
+let basicAmount = document.getElementById('basic-amount');
+let seniorAmount = document.getElementById('senior-amount');
+let totalAmount = document.getElementById('total-amount');
+const changeTotalPrice = document.querySelectorAll('.change-total-price');
+
+function checkTicketPrice() {
+  const chbox1 = document.getElementById('ticket-choice1');
+  const chbox2 = document.getElementById('ticket-choice2');
+  const chbox3 = document.getElementById('ticket-choice3');
+  if (chbox1.checked) {
+    ticketPrice = 20;
+  } else if (chbox2.checked) {
+    ticketPrice = 25;
+  } else {
+    ticketPrice = 40;
+  }
+}
+
+function countPrice() {
+  checkTicketPrice();
+  totalAmount.innerHTML = basicAmount.value * ticketPrice + seniorAmount.value * ticketPrice / 2;
+}
+
+changeTotalPrice.forEach(
+  function(n) {
+    console.log(ticketPrice)
+    n.addEventListener('click', countPrice);
+  }
+)
+
+// changeTotalPrice.addEventListener('click', countPrice);
+
