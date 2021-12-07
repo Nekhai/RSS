095cb73908ff7b9f20b61876f9980c55989dcc4a
Nekhai <NekhaiYurii@gmail.com>
2021-12-07 18:43:10 +0200

    feat: add tsconfig and eslint configs

--- migration-to-ts/webpack.config.js
+++ migration-to-ts/webpack.config.js
@@ -15,11 +15,11 @@ const baseConfig = {
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.js', '.ts'],
     },
     output: {
         filename: 'index.js',
-        path: path.resolve(__dirname, '../dist'),
+        path: path.resolve(__dirname, './dist'),
     },
     plugins: [
         new HtmlWebpackPlugin({
